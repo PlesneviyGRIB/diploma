070cdf947d3f588a5cbec4a5bde38869887e4744
PlesneviyGRIB <unstoppablerina@gmail.com>
2024-03-30 10:25:21 +0700

    fix docker image

--- sqlTool/database/restore_db.sh
+++ sqlTool/database/restore_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-psql -U admin -h localhost -p 5432 sqltool -c 'drop schema public cascade;create schema public;grant all on schema public to public;'
-psql -U admin -h localhost -p 5432 --single-transaction sqltool < ./test.sql
+psql -U admin -h localhost -p 5436 sqltool -c 'drop schema public cascade;create schema public;grant all on schema public to public;'
+psql -U admin -h localhost -p 5436 --single-transaction sqltool < ./init.sql
